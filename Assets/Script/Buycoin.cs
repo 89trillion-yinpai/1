c6fd92dfe4b790dd458c1c58333e911bb09cd4c2
尹派 <yinpai@89trillion.com>
2021-10-27 17:09:59 +0800

    第一题

---
+++ Assets/Script/Buycoin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class Buycoin : MonoBehaviour
+{
+    public Text wallet;
+    public void Hit_test(int num)//加钱方法
+    {
+        int i;
+        i =Convert.ToInt32(wallet.text);//强制类型转换成整形进行计算
+        i += num;
+        wallet.text = i.ToString();//再把原本的类型转换回来
+    }
+}
---
+++ Assets/Script/Duqu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class Duqu : MonoBehaviour
+{
+    //声明列表
+    public List<Read> item;
+    public int num;
+    
+    
+    //初始化对象
+    void Awake()
+    {
+        //读取json数据
+        TextAsset txt = Resources.Load<TextAsset>("data") as TextAsset;
+        JSONNode jsonObject = JSON.Parse(txt.text);
+        //创建对象
+        for (int i = 0; i < jsonObject[0].Count; i++)
+        {
+            Read fieldRead = new Read();
+            fieldRead.type = jsonObject[0][i]["type"];
+            fieldRead.num = jsonObject[0][i]["num"];
+            fieldRead.subType = jsonObject[0][i]["subType"];
+            fieldRead.productId = jsonObject[0][i]["productId"];
+            fieldRead.costGold = jsonObject[0][i]["costGold"];
+            fieldRead.costDiamond = jsonObject[0][i]["costDiamond"];
+            fieldRead.isPurchased = jsonObject[0][i]["isPurchased"];
+            item.Add(fieldRead);
+            num = jsonObject[0].Count;
+        }
+        
+    }
+
+}
